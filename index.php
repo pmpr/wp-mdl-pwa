f37493e10000b79f934e3f169c961ef3a0d4069d
Pmnmpr <pmnmpr@gmail.com>
2022-08-28 06:58:39 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             627f5bd203879             |
+    |             630b11fc0c95d             |
     |_______________________________________|
 */
  use Pmpr\Module\PWA\PWA; PWA::symcgieuakksimmu();
--- src/Pmpr/Module/PWA/Container.php
+++ src/Pmpr/Module/PWA/Container.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             627f5bd203879             |
+    |             630b11fc0c95d             |
     |_______________________________________|
 */
- namespace Pmpr\Module\PWA; use Pmpr\Common\Foundation\Container\Container as BaseClass; class Container extends BaseClass { const auugqowqueaocgsu = "\166\x31\x2e\61\x2e\x34"; const ekuyygygyuwsouiq = "\x2f\163\x77\55" . self::auugqowqueaocgsu . "\x2e\152\163"; const jgkmawiysugewomi = "\155\141\156\151\x66\145\x73\164\55\x25\144\x2e\167\x65\x62\x6d\x61\156\151\x66\x65\x73\164"; public function __construct() { $this->settingObj = Setting::symcgieuakksimmu(); parent::__construct(); } public function asckusockwoccusa() : string { return sprintf(self::jgkmawiysugewomi, self::auugqowqueaocgsu); } }
+ namespace Pmpr\Module\PWA; use Pmpr\Common\Foundation\Container\Container as BaseClass; class Container extends BaseClass { const megoekiemouaquwk = "\166\x31\x2e\61\x2e\x34"; const ekuyygygyuwsouiq = "\x2f\x73\167\55" . self::megoekiemouaquwk . "\56\152\163"; const jgkmawiysugewomi = "\155\x61\x6e\151\x66\145\x73\164\x2d\x25\144\x2e\167\x65\142\x6d\x61\156\151\x66\x65\163\x74"; public function __construct() { $this->settingObj = Setting::symcgieuakksimmu(); parent::__construct(); } public function asckusockwoccusa() : string { return sprintf(self::jgkmawiysugewomi, self::megoekiemouaquwk); } }
